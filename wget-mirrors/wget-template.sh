b954095a0621e5ab59f225222753a780c94fded8
chriad <chriad.mueller@protonmail.com>
2024-09-06 12:00:02 +0200

    mirror of site https://dept-info.labri.fr/~strandh/Teaching/MTP/Common/David-Lamkins

--- wget-mirrors/wget-template.sh
+++ wget-mirrors/wget-template.sh
@@ -3,17 +3,19 @@
 # possibly:
 # --span-hosts or --domains
 # mirror = -N -r -l inf
+# --directory-prefix (same as -P): where to save
 
+base="${1}"
 wget \
     --mirror \
+    --base "${base}" \
     --page-requisites \
     --convert-links \
     --random-wait \
     --no-host-directories \
     --no-parent \
     --adjust-extension \
-    -P data \
     --directory-prefix "${3}" \
     --output-file=./log.txt \
-    --cut-dirs="${1}" \
-    --continue "${2}"
+    --cut-dirs="${2}" \
+    --continue "${base}"
